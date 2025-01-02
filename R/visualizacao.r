bcef801c554b59844d1fa700e4da3a901ead9c75
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-01-02 15:55:34 -0300

    plots completos

--- R/visualizacao.r
+++ R/visualizacao.r
@@ -1,6 +1,6 @@
 library(plotly)
 
-plota_fcf <- function(f) {
+plota_fcf <- function(f, return_data = FALSE) {
 
     x1 <- x2 <- seq(0, 1, by = .01)
     X <- expand.grid(x1 = x1, x2 = x2)
@@ -8,15 +8,20 @@ plota_fcf <- function(f) {
     y <- with(X, f(x1, x2))
     y <- matrix(y, length(x1), length(x2), byrow = TRUE)
 
+    if (return_data) {
+        X <- cbind(X, y = c(t(y)))
+        return(X)
+    }
+
     p <- plot_ly(x = x1, y = x2, z = y) %>% add_surface()
     attr(p, "f") <- f
 
     invisible(p)
 }
 
-plota_corte <- function(fp, x) UseMethod("plota_corte")
+plota_corte <- function(fp, x, return_data) UseMethod("plota_corte")
 
-plota_corte.function <- function(f, x = list()) {
+plota_corte.function <- function(f, x = list(), return_data = FALSE) {
 
     x0s <- do.call(rbind, x)
     y0s <- f(x0s[, 1], x0s[, 2])
@@ -30,6 +35,16 @@ plota_corte.function <- function(f, x = list()) {
     ys <- do.call(cbind, ys)
     y_max <- apply(ys, 1, max)
     y_max <- matrix(y_max, length(x1), length(x2), byrow = TRUE)
+
+    if (return_data) {
+        X <- cbind(X, y = c(t(y_max)))
+        X$tipo <- "superficie"
+        x0s <- as.data.frame(cbind(x0s, y0s))
+        x0s$tipo <- "ponto"
+        colnames(x0s) <- colnames(X)
+        X <- rbind(X, x0s)
+        return(X)
+    }
 
     p <- plot_ly() %>% add_surface(x = x1, y = x2, z = y_max) %>%
         add_markers(x = x0s[, 1], y = x0s[, 2], z = y0s)
--- fig_fcf.r
+++ fig_fcf.r
@@ -9,11 +9,18 @@ f <- fcf_generator(0, -2, -1)
 p <- plota_fcf(f)
 saveWidget(p, "./fcf.html")
 
+d <- plota_fcf(f, TRUE)
+colnames(d) <- c("x", "y", "z")
+write.csv(d, "fcf.csv", row.names = FALSE, quote = FALSE)
+
 # CORTES
 
 x_corte1 <- c(.15, .6)
 x_corte2 <- c(.85, .6)
 p <- plota_corte(f, list(x_corte1, x_corte2)) %>%
     layout(scene = list(zaxis = list(range = c(0, 1))))
-saveWidget(p, "./cortes.html")
+saveWidget(p, "./cortes_same_x.html")
 
+d <- plota_corte(f, list(x_corte1, x_corte2), TRUE)
+colnames(d) <- c("x", "y", "z", "tipo")
+write.csv(d, "cortes.csv", row.names = FALSE, quote = FALSE)
